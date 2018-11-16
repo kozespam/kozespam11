963db0d2900565efd28b5aa9b17c122d34a24c80
kozespam <45069861+kozespam@users.noreply.github.com>
2018-11-16 14:06:55 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,16 @@
+﻿client.on('ready', async() => {
+var server = "499933149019766785"; 
+var channel = "499933149573283862";
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('كودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودز ')
+    },305);
+})
+
+client.on('ready', async() => {
+var server = "499933149019766785"; 
+var channel = "499933149573283862";
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('#daily')
+    },86400);
+})
+client.login(process.env.BOT_TOKEN);

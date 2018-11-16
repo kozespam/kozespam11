a12ff6ad94f85fe67664529dd90e7c043fef44a7
kozespam <45069861+kozespam@users.noreply.github.com>
2018-11-16 14:23:47 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,4 +1,32 @@
-﻿client.on('ready', async() => {
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(Logged in as ${client.user.tag}!);
+ client.user.setGame('s!help | *By KoZe','https://www.twitch.tv/peery13%27);
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log([Start] ${new Date()});
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(Logged in as * [ " ${client.user.username} " ]);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(servers! [ " ${client.guilds.size} " ]);
+  console.log(Users! [ " ${client.users.size} " ]);
+  console.log(channels! [ " ${client.channels.size} " ]);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+client.on('ready', async() => {
 var server = "512611319640227851"; 
 var channel = "512611319640227853";
     setInterval(()=>{
